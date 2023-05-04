78c8517498245262dc5941a01e0b9e7898d3d962
erikkoli <nissiy093091@gmail.com>
2023-05-04 20:10:23 +0900

    Rubocopの実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {Faker::Japanese::Name.last_name}
-    first_name            {Faker::Japanese::Name.name}
-    last_name_kana        {person.last.katakana}
-    first_name_kana       {person.first.katakana}
-    birthday              {Faker::Date.between(from: '1930-01-01', to: '2023-5-4')}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { Faker::Japanese::Name.last_name }
+    first_name            { Faker::Japanese::Name.name }
+    last_name_kana        { person.last.katakana }
+    first_name_kana       { person.first.katakana }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2023-5-4') }
   end
-end 
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nickname, email, password, password_confirmation, last_name, first_name, last_name_kana, first_name_kana, birthdayが存在すれば登録できる" do
+      it 'nickname, email, password, password_confirmation, last_name, first_name, last_name_kana, first_name_kana, birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
@@ -33,7 +33,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
       end
-      it "passwordが空では登録できない" do
+      it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
