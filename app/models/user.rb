4ce952555cd732d0100e3f59a62a547adffdc61d
erikkoli <nissiy093091@gmail.com>
2023-05-04 15:58:38 +0900

    エラーメッセージの表示とバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
