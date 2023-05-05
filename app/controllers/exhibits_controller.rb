140fe90485f49783a86f1618482773c1a201b880
erikkoli <nissiy093091@gmail.com>
2023-05-05 17:13:47 +0900

    コードレビューの修正①

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,4 +1,6 @@
 class ExhibitsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,9 +45,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordが英数字でないと登録できない' do
+      it '英字のみのパスワードでは登録できない' do
         @user.password = 'aaaaaa'
         @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '000000'
+        @user.password_confirmation = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'ｙopon2'
+        @user.password_confirmation = 'ｙopon2'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid")
       end
