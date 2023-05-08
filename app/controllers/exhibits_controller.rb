82a2c35de54263f8897c3393ff4c4ac369c2ce6d
erikkoli <nissiy093091@gmail.com>
2023-05-08 15:00:52 +0900

    カテゴリーの選択表示

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,6 +1,26 @@
 class ExhibitsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    @exhibits = Exhibit.order("created_at DESC")
+  end
+
+  def new
+    @exhibit = Exhibit.new
+  end
+
+  #def create
+    #@exhibit = Exhibit.new(exhibit_params)
+    #if @exhibit.save
+      #redirect_to root_path
+    #else
+      #render :new
+    #end
+  #end
+
+  private
+
+  def exhibit_params
+    params.require(:exhibit).permit(:product, :explain, :category_id)
   end
 end
--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -1,3 +1,13 @@
 class Exhibit < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipment
+
+  validates :product, :explain, :price, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
 end
