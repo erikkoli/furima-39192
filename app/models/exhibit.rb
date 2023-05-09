d968a46fcefec511bf4549d190f28ab3b2471e70
erikkoli <nissiy093091@gmail.com>
2023-05-09 16:20:43 +0900

    エラーメッセージ重複修正

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -14,7 +14,7 @@ class Exhibit < ApplicationRecord
   validates :postage_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipment_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, allow_blank: true
+  validates :price, format: { with: /\A[0-9]+\z/ }, allow_blank: true
   validates :image, presence: true
 end
