16be5af1ab9dfcf8db4bfb0b6ec1bca9a6fb99ad
erikkoli <nissiy093091@gmail.com>
2023-05-10 17:36:51 +0900

    編集・削除ボタンの表示分け

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,5 +1,5 @@
 class ExhibitsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @exhibits = Exhibit.order('created_at DESC')
