3027788ab2a884e776d76e1794f7b8948b31cb38
erikkoli <nissiy093091@gmail.com>
2023-05-11 17:28:04 +0900

    編集機能の実装

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,5 +1,7 @@
 class ExhibitsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_exhibit, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @exhibits = Exhibit.order('created_at DESC')
@@ -19,10 +21,17 @@ class ExhibitsController < ApplicationController
   end
 
   def show
-    @exhibit = Exhibit.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @exhibit.update(exhibit_params)
+      redirect_to exhibit_path(@exhibit)
+    else
+      render :edit
+    end
   end
 
   private
@@ -31,4 +40,13 @@ class ExhibitsController < ApplicationController
     params.require(:exhibit).permit(:product, :explain, :price, :category_id, :condition_id, :postage_id, :prefecture_id,
                                     :shipment_id, :image).merge(user_id: current_user.id)
   end
+
+  def set_exhibit
+    @exhibit = Exhibit.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @exhibit.user
+  end
+
 end
