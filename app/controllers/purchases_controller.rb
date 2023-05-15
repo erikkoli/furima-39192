d33bf853bb32c1757e32ccebe86139d2c0b87bf1
erikkoli <nissiy093091@gmail.com>
2023-05-15 14:59:14 +0900

    購入のためのフォームを実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,9 @@
 class PurchasesController < ApplicationController
   def index
+    @exhibit = Exhibit.find(params[:exhibit_id])
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new
   end
 end
