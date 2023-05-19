d4c43481f744aefb4b7ac5f122a1d9a53cec9c7f
erikkoli <nissiy093091@gmail.com>
2023-05-19 15:34:47 +0900

    鍵情報を環境変数として設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -35,7 +35,7 @@ class PurchasesController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_*************************"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @exhibit.price,
       card: purchase_params[:token],
